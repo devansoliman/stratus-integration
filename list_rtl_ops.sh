655250224923037b79a393ef1f11673e84ba8e4c
Devan Soliman <95260363+devansoliman@users.noreply.github.com>
2025-11-28 22:40:55 -0800

    Change snapshot from 'rtl' to 'optim'

--- list_rtl_ops.sh
+++ list_rtl_ops.sh
@@ -1,5 +1,5 @@
 module load stratus/latest
 stratus
 open_project project.tcl
-load_results hls_config:simple_p2p/dut/BASIC -snapshot rtl
+load_results hls_config:simple_p2p/dut/BASIC -snapshot optim
 find -op
